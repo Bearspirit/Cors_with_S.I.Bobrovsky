c4d71a7bd37175ab4a8e1f8a980d7b232ac97272
Mikhail Voitovich <mihailvoitovich@gmail.com>
2021-05-07 11:55:55 +0300

    change test for task 3

--- 28_tasks/main_course/task_3_test.py
+++ 28_tasks/main_course/task_3_test.py
@@ -1,15 +1,14 @@
 import unittest
-import random
 from task_3 import TheRabbitsFoot
 
 class func_test(unittest.TestCase):
-    def encode_test(self):
+    def test_encode(self):
         self.assertEqual(TheRabbitsFoot("отдай мою кроличью лапку", True), "омоюу толл дюиа акчп йрьк")
-        self.assertEqual(TheRabbitsFoot("я был сегодня в деревне", True), "яеяе бгвв ыодн ядее снр")
+        self.assertEqual(TheRabbitsFoot("я был сегодня в деревне", True), "яеяе бгвв ыодн лдее снр")
 
-    def decode_test(self):
+    def test_decode(self):
         self.assertEqual(TheRabbitsFoot("омоюу толл дюиа акчп йрьк", False), "отдаймоюкроличьюлапку")
-        self.assertEqual(TheRabbitsFoot("яеяе бгвв ыодн ядее снр", False), "ябылсегоднявдеревне")
+        self.assertEqual(TheRabbitsFoot("яеяе бгвв ыодн лдее снр", False), "ябылсегоднявдеревне")
        
 
 if __name__ == '__main__':
