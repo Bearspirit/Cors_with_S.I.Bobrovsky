826038c1a4dfd86a444bf3dadfbf6e07ee81121e
Mikhail Voitovich <mihailvoitovich@gmail.com>
2021-07-15 23:55:51 +0300

    add task 16

---
+++ 28_tasks/main_course/task_16.py
@@ -0,0 +1,59 @@
+"""
+Сама суть этой загадки и то возбужденное состояние, в котором пребывал клиент Холмса, 
+придавали этому делу необычный характер. Да и, кроме предстоящего расследования, мастерство моего друга, 
+его умение удивительно быстро овладевать ситуацией и на основании тщательных наблюдений и простой логики 
+делать поразительные по своей точности выводы зачаровывали меня. Изучать систему его работы и приемы, 
+с помощью которых он в два счета распутывал сложнейшие загадки, для меня было настоящим удовольствием.
+
+
+Шерлок Холмс в свободное время упражняется в проверке валидных паролей к его новой механической шкатулке. 
+Пароли строятся из латинских букв и считаются валидными, если в соответствующей строке пароля все буквы 
+встречаются одинаковое количество раз. Кроме того, разрешается удалить одну любую букву, чтобы выполнилось 
+условие равенства частоты всех букв.
+
+Например, строка xyz будет валидна, и строка xyzaa будет валидна (можно удалить одну a), и строка xxyyz 
+будет валидна (можно удалить z). А строка xyzzz, или строка xxyyza или строка xxyyzabc невалидны.
+
+Напишите функцию, проверяющую строку на валидность.
+
+Функция
+
+boolean SherlockValidString(string s)
+получает на вход исходную строку длиной 2 или более английских букв, и возвращает true, если строка валидна.
+"""
+
+def SherlockValidString(der_parol):
+    main_list = []
+    midle_dict = {}
+    midle_list = []
+    result_list = []
+
+    for i in range(0, len(der_parol)):
+        main_list.append(der_parol[i])
+    elements_set = set(main_list)
+    for symbol in elements_set:
+        midle_dict[symbol] = main_list.count(symbol)
+    for value in midle_dict.values():
+        midle_list.append(value)
+    if 1 in midle_list:
+        if midle_list.count(1) == 1:
+            midle_list.remove(1)
+    result_set = set(midle_list)
+
+    if len(result_set) == 1:
+        return True
+    elif len(result_set) > 2:
+        return False
+    for sym in result_set:
+        result_list.append(midle_list.count(sym))
+    
+    if 1 not in result_list:
+        return False
+    else:
+        for el in result_set:
+            if midle_list.count(el) == 1:
+                midle_list.remove(el)
+                if (el-1) == midle_list[0]:
+                    return True    
+                else:
+                    return False 
---
+++ 28_tasks/main_course/task_16_test.py
@@ -0,0 +1,25 @@
+import unittest
+from task_16 import SherlockValidString
+
+class SherlockValidString_test(unittest.TestCase):
+    def test_der_parol_is_True(self):
+        self.assertTrue(SherlockValidString("xyz"))
+        self.assertTrue(SherlockValidString("xyzaa"))
+        self.assertTrue(SherlockValidString("xxyyz"))
+        self.assertTrue(SherlockValidString("xxyyzziii"))
+        self.assertTrue(SherlockValidString("xyzxyzt"))
+        self.assertTrue(SherlockValidString("xzzz"))
+        self.assertTrue(SherlockValidString("xz"))
+
+
+    def test_der_parol_is_False(self):
+        self.assertFalse(SherlockValidString("xyzzz"))
+        self.assertFalse(SherlockValidString("xxyyza"))
+        self.assertFalse(SherlockValidString("xxyyzabc"))
+        self.assertFalse(SherlockValidString("xxyyzzzz"))
+        self.assertFalse(SherlockValidString("xxyyzzziiioo"))
+        self.assertFalse(SherlockValidString("xxzzzz"))
+
+
+if __name__ == '__main__':
+    unittest.main()
