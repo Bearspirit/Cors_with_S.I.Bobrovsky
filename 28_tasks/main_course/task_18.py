5f41535acfc1a33591e9edeae7e4d805866ef8fc
Mikhail Voitovich <mihailvoitovich@gmail.com>
2021-07-28 00:13:29 +0300

    change task 18

--- 28_tasks/main_course/task_18.py
+++ 28_tasks/main_course/task_18.py
@@ -31,13 +31,54 @@ MatrixTurn(["123456", "234567", "345678", "456789"], 4,6, 3)
 """
 
 def MatrixTurn(Matrix, M, N, T):
-    matrix_list = []
+    
+    matrix = []
+    midle_list = []
     for i in range(0, M):
-        matrix_list.append([])
+        matrix.append([])
+        midle_list.append([])
         for j in range(0, N):
-            matrix_list[i].append(Matrix[i][j])
-    return matrix_list
+            matrix[i].append(Matrix[i][j])
+            midle_list[i].append(Matrix[i][j])
+
+    for k in range(0, T):
+        l = -1
+        for m in range(0, M//2):
+            b = M - m - 1
+            midle_list[m].insert(0,matrix[m+1][0])
+            del midle_list[m][-1]
+            del midle_list[b][0]
+            midle_list[b].append(matrix[b-1][-1])
+            
+
+
+            
+
+    """
+    size = len(matrix)
+    layer_count = size / 2
+
+    for layer in range(0, int(layer_count)):
+        first = layer
+        last = size - first - 1
+
+        for element in range(first, last):
+            offset = element - first
+
+            top = matrix[first][element]
+            right_side = matrix[element][last]
+            bottom = matrix[last][last-offset]
+            left_side = matrix[last-offset][first]
+
+            matrix[first][element] = left_side
+            matrix[element][last] = top
+            matrix[last][last-offset] = right_side
+            matrix[last-offset][first] = bottom
+
+    """
+    
+    return midle_list
 
 
 
-print(MatrixTurn(["123456", "234567", "345678", "456789"], 4,6, 3))
+print(MatrixTurn(["123456", "234567", "345678", "456789"], 4, 6, 1))
