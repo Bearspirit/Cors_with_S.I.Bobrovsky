ebb861172f947a54c157ccb2d60e89fd798dfaf5
Mikhail Voitovich <mihailvoitovich@gmail.com>
2021-11-18 19:34:16 +0300

    change PowerSet

--- Algorithms_and_data_structures/PowerSet/PowerSet.py
+++ Algorithms_and_data_structures/PowerSet/PowerSet.py
@@ -1,63 +1,16 @@
-class HashTable:
-    def __init__(self, sz, stp):
-        self.sizex = sz
-        self.step = stp
-        self.slots = [None] * self.sizex
+class PowerSet():
 
-    def hash_fun(self, value):
-        return sum(value.encode('utf-8')) % self.sizex
-
-    def seek_slot(self, value):
-        index = self.hash_fun(value)
-        position_count = index
-        iter_count = self.sizex
-        while iter_count >= 0:
-            if self.slots[position_count] == None:
-                return position_count
-            elif self.slots[position_count] == value:
-                return position_count
-            else:
-                position_count += self.step
-                if position_count > (self.sizex-1):
-                    position_count -= self.sizex
-            iter_count -= 1
-        # находит индекс пустого слота для значения, или None
-        return None
-
-    def put(self, value):
-        if self.seek_slot(value) != None:
-            self.slots[self.seek_slot(value)] = value
-            return self.seek_slot(value)
-        return None
-         # записываем значение по хэш-функции
-         
-         # возвращается индекс слота или None,
-         # если из-за коллизий элемент не удаётся
-         # разместить 
-
-    def find(self, value):
-        find_index = self.seek_slot(value)
-        if find_index !=None:
-            if self.slots[find_index] == value:
-                return find_index
-         # находит индекс слота со значением, или None
-        return None
-
-class PowerSet(HashTable):
-
-    def __init__(self, sz, stp):
-        super(PowerSet, self).__init__(sz, stp)
+    def __init__(self):
+        self.slots = []
         # ваша реализация хранилища
 
     def size(self):
-        count = 0
-        for i in range(0, self.sizex):
-            if self.slots[i] != None:
-                count += 1
-        return count
+        return len(self.slots)
         # количество элементов в множестве
 
-    #def put(self, value):
+    def put(self, value):
+        if value not in self.slots:
+            self.slots.append(value)
         # всегда срабатывает
 
     def get(self, value):
@@ -69,54 +22,45 @@ class PowerSet(HashTable):
 
     def remove(self, value):
         if value in self.slots:
-            self.slots[self.slots.index(value)] = None
+            self.slots.pop(self.slots.index(value))
             return True
         # возвращает True если value удалено
         # иначе False
         return False
 
     def intersection(self, set2):
-        cross_set = []
-        for elements in set2:
-            if elements in self.slots:
-                cross_set.append(elements)
+        cross_set = PowerSet()
+        for element in self.slots:
+            if set2.get(element):
+                cross_set.put(element)
         return cross_set            
         # пересечение текущего множества и set2
 
 
     def union(self, set2):
-        union_set = []
-        for el in self.slots:
-            if el != None:
-                union_set.append(el)
-        for elements in set2:
-            if elements not in union_set:
-                union_set.append(elements)
-        if len(union_set) > 0:
-            return union_set
+        union_set = PowerSet()
+        union_set.slots = self.slots.copy()
+        for element in set2.slots:
+            union_set.put(element)
+    
+        return union_set
         # объединение текущего множества и set2
-        return None
 
     def difference(self, set2):
-        diff_set = []
-        for elements in self.slots:
-            if (elements in set2) or (elements == None):
-                continue
-            diff_set.append(elements)
+        diff_set = PowerSet()
+        diff_set.slots = self.slots.copy()
+        for element in set2.slots:
+            diff_set.remove(element)
         return diff_set
         # разница текущего множества и set2
 
 
     def issubset(self, set2):
-        for elements in set2:
-            if elements not in self.slots:
+        for elements in set2.slots:
+            if self.get(elements) == False:
                 return False
         # возвращает True, если set2 есть
         # подмножество текущего множества,
         # иначе False
         return True
-
-    def put(self, value):
-        if not self.get(value):
-            return self.seek_slot(value)
 
--- Algorithms_and_data_structures/PowerSet/PowerSet_test.py
+++ Algorithms_and_data_structures/PowerSet/PowerSet_test.py
@@ -1,5 +1,5 @@
 import unittest
-from test_set import *
+from PowerSet import *
 
 class TestPowerSetMethods(unittest.TestCase):
     def test_put_alot_elements(self):
@@ -8,6 +8,186 @@ class TestPowerSetMethods(unittest.TestCase):
             PS.put(str(i))
         self.assertEqual(PS.size(), 20000)
 
+    def test_put_double_element(self):
+        PS = PowerSet()
+        PS.put('1')
+        PS.put('1')
+        PS.put('1')
+        self.assertEqual(PS.size(), 1)
+
+    def test_intersection_full_cross(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('1')
+        set2.put('2')
+        set2.put('3')
+        cross_set = set1.intersection(set2)
+        self.assertEqual(cross_set.size(), 3)
+
+    def test_intersection_empty_cross(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('4')
+        set2.put('5')
+        set2.put('6')
+        cross_set = set1.intersection(set2)
+        self.assertEqual(cross_set.size(), 0)
+
+    def test_intersection_part_cross(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('1')
+        set2.put('3')
+        set2.put('6')
+        cross_set = set1.intersection(set2)
+        self.assertEqual(cross_set.size(), 2)
+
+    def test_union_for_same_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('1')
+        set2.put('2')
+        set2.put('3')
+        union_set = set1.union(set2)
+        self.assertEqual(union_set.size(), 3)
+
+    def test_union_for_empty_set(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        union_set = set1.union(set2)
+        self.assertEqual(union_set.size(), 3)
+
+    def test_union_for_part_same_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('1')
+        set2.put('2')
+        set2.put('4')
+        set2.put('5')
+        union_set = set1.union(set2)
+        self.assertEqual(union_set.size(), 5)
+
+    def test_union_for_absolutly_different_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('6')
+        set2.put('7')
+        set2.put('4')
+        set2.put('5')
+        union_set = set1.union(set2)
+        self.assertEqual(union_set.size(), 7)
+
+    def test_difference_for_same_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('1')
+        set2.put('2')
+        set2.put('3')
+        diff_set = set1.difference(set2)
+        self.assertEqual(diff_set.size(), 0)
+
+    def test_difference_for_part_same_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('4')
+        set2.put('2')
+        set2.put('6')
+        diff_set = set1.difference(set2)
+        self.assertEqual(diff_set.size(), 2)
+
+    def test_difference_for_differents_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('4')
+        set2.put('5')
+        set2.put('6')
+        diff_set = set1.difference(set2)
+        self.assertEqual(diff_set.size(), 3)
+
+    def test_issubset_for_differents_sets(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('4')
+        set2.put('5')
+        set2.put('6')
+        self.assertFalse(set1.issubset(set2))
+
+    def test_issubset_for_all_set2elements_in_set1(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set1.put('4')
+        set1.put('5')
+        set1.put('6')
+        set2 = PowerSet()
+        set2.put('4')
+        set2.put('5')
+        set2.put('6')
+        self.assertTrue(set1.issubset(set2))
+
+    def test_issubset_for_all_set1elements_in_set2(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set2 = PowerSet()
+        set2.put('1')
+        set2.put('2')
+        set2.put('3')
+        set2.put('4')
+        set2.put('5')
+        set2.put('6')
+        self.assertFalse(set1.issubset(set2))
+
+def test_issubset_if_not_all_set2elements_in_set1(self):
+        set1 = PowerSet()
+        set1.put('1')
+        set1.put('2')
+        set1.put('3')
+        set1.put('4')
+        set1.put('5')
+        set1.put('6')
+        set2 = PowerSet()
+        set2.put('4')
+        set2.put('5')
+        set2.put('6')
+        set2.put('7')
+        self.assertFalse(set1.issubset(set2))
+ 
 
 if __name__ == '__main__':
     unittest.main()
